7b9bc0080ef931984e1518ffa2d2a64d5b571102
elieldioba <elieldiobat@gmail.com>
2026-04-22 14:14:40 +0100

    absence.route : ajout d'un get pour afficher toutes les absences des etudiants

--- src/routes/absence.routes.js
+++ src/routes/absence.routes.js
@@ -7,6 +7,7 @@ const { verifierToken, autoriser } = require('../middlewares/auth.middleware');
 
 router.use(verifierToken);
 
+router.get('/etudiants', listerAbsences);
 router.get('/:etudiantId', listerAbsences);
 router.post('/', autoriser('ADMINISTRATEUR', 'SECRETARIAT_PEDAGOGIQUE'), saisirAbsence);
 router.delete('/:id', autoriser('ADMINISTRATEUR', 'SECRETARIAT_PEDAGOGIQUE'), supprimerAbsence);
